ddfd25927ab133a131a1eb79509bfa05fb9824dd
Arselan <arselan95@gmail.com>
2020-11-02 00:37:23 -0800

    Add files via upload

---
+++ 283assignment2/testassignment2.c
@@ -0,0 +1,35 @@
+/*
+ * Assignment 2 test program
+ *
+ * cc -Wall -Wextra -o test_assignment2 test_assignment2.c
+ */
+
+#include <stdio.h>
+
+#include <sys/types.h>
+
+static inline void
+__cpuid(unsigned int *eax, unsigned int *ebx, unsigned int *ecx)
+{
+      asm volatile("cpuid"
+            : "=a" (*eax),
+              "=b" (*ebx),
+              "=c" (*ecx)
+            : "0" (*eax), "1" (*ebx), "2" (*ecx));
+}
+
+int
+main(int argc, char **argv)
+{
+	unsigned int eax, ebx, ecx;
+
+	eax=0x0;
+	__cpuid(&eax, &ebx, &ecx);
+	printf("CPUID(0x4FFFFFFF) EXITS = %d\n",(eax & 0x4FF));
+	printf("CYCLES IN EXIT = %d%d\n",
+			(ecx & 0xFFFFFFFF),
+			((ebx & 0xFFFFFFFF) >> 24));
+
+}
+	
+	
